eacb43db8cdca8d46f6d22a4cf237b53247f8788
alejomar <demonmaar@gmail.com>
2020-10-27 12:10:01 -0500

    Has been organized all files in respective folders, and now it relatives to root

--- js/Home/gridcards.js
+++ js/Home/gridcards.js
@@ -7,10 +7,9 @@ const card = $("card").content;
 function createcard(titletext,descripciontext,imgname)
 {
     var newcard = card.cloneNode(true);
-    newcard.querySelector("img").src= "icontest/"+imgname+".svg";
+    newcard.querySelector("img").src= "Image/Home/"+imgname+".svg";
     newcard.querySelector("h2").textContent= titletext;
     newcard.querySelector("p").textContent= descripciontext;
     porfolio.appendChild(newcard);
 }
 
-//window.customElements.define("card-app",cardApp)
