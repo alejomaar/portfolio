665723f3c38d1204d28f056cfe85392e4128de6c
alejomar <demonmaar@gmail.com>
2020-11-05 21:45:31 -0500

    The href link in python flask in home page to go program pages already works

--- prueba.py
+++ prueba.py
@@ -4,11 +4,15 @@ app =Flask(__name__)
 
 @app.route('/')
 def home():
+    #return render_template('program/'+name)
     return render_template('index.html')
 
-@app.route('/program')
-def program():
-    return render_template('program.html')
+@app.route('/app/<string:name>')
+def program(name='escalagrises.html'):
+    return render_template('program/'+name)
+    #return name;
+    #return 'File: '+name
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
