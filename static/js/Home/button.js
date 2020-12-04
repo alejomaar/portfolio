b907dfca7b5df4d8c79c6c9f6aa273d0b8610e2a
alejomar <demonmaar@gmail.com>
2020-12-03 19:52:24 -0500

    Buttons of index, now are functionals¡

--- static/js/Home/button.js
+++ static/js/Home/button.js
@@ -1,6 +1,49 @@
-function buttonmenu(button)
+//Getting Buttons 
+var buttons = document.querySelectorAll("button");
+//Add Click listener to all buttons 
+buttons.forEach(element =>{
+    element.addEventListener("click",()=> {
+        //Select and remove Active Button
+        var activeelement = document.querySelectorAll(".activebutton")[0];
+        activeelement.classList.remove("activebutton");
+        //Set actual Active Button
+        element.classList.add("activebutton");
+        filter(element);
+    });
+});
+
+function filter(button)
+{
+    var Programacion = document.querySelectorAll('[name=Programacion]');
+    var Arte = document.querySelectorAll('[name=Arte]');
+    if(button.id=='FilterArte'){  
+        showcard(Arte);
+        hidecard(Programacion)
+    }else if(button.id=='FilterProgramacion'){  
+        showcard(Programacion);
+        hidecard(Arte)
+    }else if(button.id=='FilterNone'){ 
+        showcard(Arte); 
+        showcard(Programacion);
+    }
+}
+function showcard(cards)
+{
+    cards.forEach(card => {
+        card.style.display="block";
+    });
+}
+function hidecard(cards)
+{
+    cards.forEach(card => {
+        card.style.display="none";
+        
+    });
+}
+
+/*function buttonmenu(button)
 {
     var activeelement = document.getElementsByClassName("activebutton");
     activeelement[0].classList.remove("activebutton");
     button.classList.add("activebutton");
-}
+}*/
